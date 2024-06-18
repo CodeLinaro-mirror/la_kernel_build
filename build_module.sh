0af94242ab6f8dde5767b54aff9cb07b001fae29
yuanjiey <quic_yuanjiey@quicinc.com>
2024-06-18 13:52:55 +0800

    build_module.sh: use kernel target to replace TARGET_BOARD_PLATFORM
    prepare_vendor.sh export kernel target to file kernel_target,
    build_module.sh read kernel_target file to get kernel target
    to replace TARGET_BOARD_PLATFORM.
    
    Change-Id: I8b44c260a9797d258cae94ec750404d42db0dd13
    Signed-off-by: yuanjiey <quic_yuanjiey@quicinc.com>

--- build_module.sh
+++ build_module.sh
@@ -279,13 +279,8 @@ for EXT_MOD in ${EXT_MODULES}; do
     fi
   done
 
-  if [ "$TARGET_BOARD_PLATFORM" = "msmnile" ]; then
-     btgt="gen3auto"
-  elif [ "$TARGET_BOARD_PLATFORM" = "sm6150" ]; then
-     btgt="sdmsteppeauto"
-  else
-     btgt="$TARGET_BOARD_PLATFORM"
-  fi
+  # Get kernel target from file which exported from prepare_vendor.sh
+  btgt=$(head -n 1 "${KERNEL_KIT}"/kernel_target)
 
   filter_regex="${btgt/_/-}_${VARIANT/_/-}_${SUBTARGET_REGEX:-.*}_dist$"
 
