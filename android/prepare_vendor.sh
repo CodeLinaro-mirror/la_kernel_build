f75a6acf3cf012fcfae2dfc8f16948ad0ffc918d
Satya Durga Srinivasu Prabhala <quic_satyap@quicinc.com>
2024-07-01 23:08:41 -0700

    android: prepare_vendor: split clean-up of tree for Android builds
    As the script gets executed from different directories, bazel-cache
    directory might not always gets created under Android Top directory.
    
    To avoid build issues due to missing bazel-cache under Android Top
    directory, split the clean-up of tree for Android builds where
    removal of the Android build files done separately under
    ${ANDROID_BUILD_TOP}/bazel-cache and Kernel Platform Top directory.
    
    Change-Id: I9a16affcce76903d5c01bf407da68a90daeeb804
    Signed-off-by: Satya Durga Srinivasu Prabhala <quic_satyap@quicinc.com>

--- android/prepare_vendor.sh
+++ android/prepare_vendor.sh
@@ -435,22 +435,23 @@ if [ -n "${ANDROID_PRODUCT_OUT}" ] && [ -n "${ANDROID_BUILD_TOP}" ]; then
   echo
   echo "  cleaning up kernel_platform tree for Android"
 
+  set -x
   # When --sandbox_debug is passed as arg to Bazel, inaccessibleHelperDir gets
   # created with limited perms. Because of which find command fails and leads
   # to build failure in vendor builds. Update the perms to make sure find cmd
   # goes through w/o any issues.
   if [ -d "${ANDROID_BUILD_TOP}/bazel-cache" ]; then
     chmod -R 0755 "${ANDROID_BUILD_TOP}/bazel-cache"
+    find "${ANDROID_BUILD_TOP}/bazel-cache" \( -name Android.mk -o -name Android.bp \) \
+      -a -not -path "${ROOT_DIR}"/msm-kernel/Android.bp -delete
   fi
 
   if [ -d "${ROOT_DIR}/bazel-cache" ]; then
     chmod -R 0755 "${ROOT_DIR}/bazel-cache"
   fi
 
-  set -x
-  find "${ROOT_DIR}" "${ANDROID_BUILD_TOP}/bazel-cache" \( -name Android.mk -o -name Android.bp \) \
-    -a -not -path "${ROOT_DIR}"/common/Android.bp -a -not -path "${ROOT_DIR}"/msm-kernel/Android.bp \
-    -delete
+  find "${ROOT_DIR}" \( -name Android.mk -o -name Android.bp \) \
+    -a -not -path "${ROOT_DIR}"/msm-kernel/Android.bp -delete
   set +x
 
   ################################################################################
