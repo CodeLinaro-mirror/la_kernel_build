3dc46ebd77fc8a69c2e7769252cd85ba544fd8cd
Houffaneh Osman <hosman@qti.qualcomm.com>
2025-05-19 13:22:07 +0200

    build_trout_kernel.sh: Add ability to build dtb needed later by vm-boot generation
    Change-Id: Ib3fc335047791daaaca7ec7357f79e375115195e
    Signed-off-by: Houffaneh Osman <hosman@qti.qualcomm.com>

--- build_trout_kernel.sh
+++ build_trout_kernel.sh
@@ -13,20 +13,49 @@ cleanup() {
 trap cleanup INT ERR
 
 BASE=$(dirname $(dirname $(dirname $(readlink -f $0))))
+KERNEL_VENDOR_DTS_PATH=$BASE/kernel_platform/msm-kernel/arch/arm64/boot/dts/vendor
+KERNEL_INCLUDE_PATH=$BASE/kernel_platform/msm-kernel/include
+KERNEL_OUT=$BASE/device/common/kalama-kernel
 
-echo $BASE
+mkdir -p $KERNEL_OUT
+
+echo "BASE=$BASE"
+echo "KERNEL_OUT=$KERNEL_OUT"
+
+rm -rf $KERNEL_OUT
 
 pushd $BASE > /dev/null
 
 rm -f WORKSPACE
 
 ln -s build/kernel/kleaf/bazel.WORKSPACE WORKSPACE
-rm -rf kernel/trout_virtio_kernel
 
 tools/bazel clean --expunge
 
-tools/bazel run //common-modules/virtual-device:virtual_device_aarch64_dist  -- --dist_dir=kernel/trout_virtio_kernel
+tools/bazel run //common-modules/virtual-device:virtual_device_aarch64_dist  -- --dist_dir=$KERNEL_OUT
 
 rm -f WORKSPACE
+
+# Generate kalama-vm-cdp.dtb needed for by ghgvm-pilsplitter.sh tool.
+
+IDE=kalama-vm-cdp
+SRC=$KERNEL_VENDOR_DTS_PATH/qcom/$IDE.dts
+TMP=$KERNEL_OUT/$IDE.tmp.dts
+DST=$KERNEL_OUT/dtbs/$IDE.dtb
+
+rm -rf $KERNEL_OUT/dtbs
+mkdir -p $KERNEL_OUT/dtbs
+
+cpp -nostdinc -I $KERNEL_INCLUDE_PATH -undef -x assembler-with-cpp $SRC > $TMP
+dtc -O dtb -b 0 -o $DST $TMP
+
+rm -f $TMP
+
+#Remove all Android.mk and Android.bp files from source so they don't conflict with Android's
+#As not building for the current component in these folders, have no affect but references.
+find ${BASE}/vendor/qcom \( -name Android.mk -o -name Android.bp \) \
+    -a -not -path ${BASE}/common/Android.bp -a -not -path ${BASE}/kernel_platform/msm-kernel/Android.bp \
+    -delete
+set +x
 
 popd > /dev/null
