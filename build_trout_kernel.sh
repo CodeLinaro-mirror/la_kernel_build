811b3d585e87deaa0a1c73336abf41b6286430e6
Houffaneh Osman <hosman@qti.qualcomm.com>
2025-05-19 20:29:44 +0200

    build_trout_kernel.sh: Adding ability to not recompile kernel and reuse
    RECOMPILE_KERNEL=0 ./kernel/build/build_trout_kernel.sh will not clear
    and clean previously built kernel images
    
    Change-Id: I4b89e7a83f581f3cdf2bd67d5d68a187a4d44362
    Signed-off-by: Houffaneh Osman <hosman@qti.qualcomm.com>

--- build_trout_kernel.sh
+++ build_trout_kernel.sh
@@ -22,15 +22,21 @@ mkdir -p $KERNEL_OUT
 echo "BASE=$BASE"
 echo "KERNEL_OUT=$KERNEL_OUT"
 
-rm -rf $KERNEL_OUT
-
 pushd $BASE > /dev/null
 
+rm -rf $KERNEL_OUT
 rm -f WORKSPACE
 
 ln -s build/kernel/kleaf/bazel.WORKSPACE WORKSPACE
 
-tools/bazel clean --expunge
+if [ "${RECOMPILE_KERNEL}" == "1" ]; then
+    # remove all trace of previously built kernel
+    echo "cleaning previously built kernel"
+    tools/bazel clean --expunge
+else
+    #bazel tool will simply just copy a previously built kernel to the target directory.
+    echo "re-using previous build"
+fi
 
 tools/bazel run //common-modules/virtual-device:virtual_device_aarch64_dist  -- --dist_dir=$KERNEL_OUT
 
