211a42ed17ab14f2a14b550f511c5fb2e61311eb
Song Xue <quic_songxue@quicinc.com>
2025-05-21 10:53:11 +0800

    prepare_vendor: Add support for building canoe
    Add support for building canoe kernel image as the kernel chipset
    name is different from canoe.
    
    Change-Id: I38e547148cb34e074bffadfbd82fe40bca296c8f
    Signed-off-by: Song Xue <quic_songxue@quicinc.com>

--- android/prepare_vendor.sh
+++ android/prepare_vendor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
-# Copyright (c) 2022-2024, Qualcomm Innovation Center, Inc. All rights reserved.
+# Copyright (c) 2022-2025, Qualcomm Innovation Center, Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -138,6 +138,9 @@ fi
 case "${KERNEL_TARGET}" in
   taro)
     KERNEL_TARGET="waipio"
+    ;;
+  canoe)
+    KERNEL_TARGET="x1p42100"
     ;;
 esac
 
