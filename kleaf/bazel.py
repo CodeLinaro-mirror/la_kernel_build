3a8b382806d1e5ee32b23483db0543b25968fd0d
HONG Yifan <elsk@google.com>
2025-06-11 19:32:00 +0000

    kleaf: Fix --source_date_epoch_fix.
    It needs to call bazel info output_base without
    the kleaf_repo_dir flag.
    
    Also adds command != "info" check to prevent infinite recursion.
    
    Bug: 377954908
    Change-Id: I74819432086379bb10c87d43754d8718affb0877

--- kleaf/bazel.py
+++ kleaf/bazel.py
@@ -490,7 +490,7 @@ class BazelWrapper(KleafHelpPrinter):
         repo_root, repo_manifest = self.known_args.repo_manifest
         self.env["KLEAF_REPO_MANIFEST"] = f"{repo_root or ''}:{repo_manifest or ''}"
 
-        if self.known_args.source_date_epoch_fix:
+        if self.known_args.source_date_epoch_fix and self.command != "info":
             self.env["KLEAF_OUTPUT_BASE"] = self._get_output_base()
 
         if self.known_args.extra_git_projects:
@@ -641,7 +641,7 @@ class BazelWrapper(KleafHelpPrinter):
         """Returns $(bazel info output_base)"""
 
         return subprocess.check_output(
-            [sys.executable, __file__, self.kleaf_repo_dir] +
+            [sys.executable, __file__] +
             self.startup_options +
             ["info", "output_base"], text=True).strip()
 
