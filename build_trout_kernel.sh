6d1c7cb8af6518ef6ef4ba6a703353a6e30c8378
Houffaneh Osman <hosman@qti.qualcomm.com>
2025-07-17 15:56:28 +0200

    build_trout_kernel.sh: Error checking with trap INT/ERR for failure
    Change-Id: I0d5da0eb105e0a60be41a5567344353198f78bdd
    Signed-off-by: Houffaneh Osman <hosman@qti.qualcomm.com>

--- build_trout_kernel.sh
+++ build_trout_kernel.sh
@@ -4,13 +4,21 @@
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 
 #set -x
+
+cleanup() {
+    printf "Received ERR/INT....Exiting build_trout_kernel.sh\n"
+    exit 1
+}
+
+trap cleanup INT ERR
+
 BASE=$(dirname $(dirname $(dirname $(readlink -f $0))))
 
 echo $BASE
 
 pushd $BASE > /dev/null
 
-rm WORKSPACE
+rm -f WORKSPACE
 
 ln -s build/kernel/kleaf/bazel.WORKSPACE WORKSPACE
 rm -rf kernel/trout_virtio_kernel
@@ -19,6 +27,6 @@ tools/bazel clean --expunge
 
 tools/bazel run //common-modules/virtual-device:virtual_device_aarch64_dist  -- --dist_dir=kernel/trout_virtio_kernel
 
-rm WORKSPACE
+rm -f WORKSPACE
 
 popd > /dev/null
