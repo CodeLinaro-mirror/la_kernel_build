ab96c17674de240897e5fcda32fb0071a52ea084
HONG Yifan <elsk@google.com>
2025-11-14 02:14:20 +0000

    bazel: allow parallel execution for bazel.py.
    When the user issues multiple bazel commands simultanously, before this
    change, these `bazel.py` executions may write to the same
    generated.bazelrc files simultanously, entering a race condition. For
    example:
    
        $ bazel build X &  # Process 1
        $ bazel build X &  # Process 2
    
    The following sequence may occur: 1. Process 1 finishes writing
    generated.bazelrc 2. Process 2 opens generated.bazelrc with mode "w",
    truncating the file 3. Process 1 executes `bazel`, reading the empty
    generated.bazelrc file 4. Process 1 fails
    
    To solve this issue, a fnctl.lockf(LOCK_EX) is placed on
    out/bazel/bazelrc/lockfile before writing the bazelrc file and Bazel
    reading the bazelrc file. The lock is released at process exit.
    
    `bazel clean` does not delete the lockfile. Otherwise, other processes
    could enter a race condition (See bazelrc_writer_test,
    test_multi_process).
    
    Bug: 452382967
    Change-Id: Ib63d194c9735168619fa1ee2b2759e224acb475a

--- kleaf/bazelrc_writer.py
+++ kleaf/bazelrc_writer.py
@@ -15,28 +15,156 @@
 """Helper classes to support writing bazelrc files."""
 
 import dataclasses
+import errno
+import fcntl
+import os
 import pathlib
-import shutil
+import random
+import sys
+import time
 from typing import TextIO
+
+_WARN_SECONDS = 10
+_WAIT_RECHECK_MAX_SECONDS = 1
+
+
+@dataclasses.dataclass
+class _Lockf(object):
+    """Wraps a lockf() call.
+
+    Despite the name, the class itself is not thread-safe.
+
+    The lock can only be successfully acquire()d once then release()d once.
+    After release(), it cannot be acquire()d again.
+    """
+    lockfile: pathlib.Path
+
+    def __post_init__(self):
+        self._lock_fd = os.open(self.lockfile, os.O_WRONLY | os.O_CREAT)
+
+        # Intentionally disable FD_CLOEXEC so that the execve won't
+        # release the lock. The lock is released:
+        # - If execve(), when the process exits
+        # - If using subprocess, when BazelWrapper.__exit__
+        flags = fcntl.fcntl(self._lock_fd, fcntl.F_GETFD)
+        flags = flags & ~fcntl.FD_CLOEXEC
+        fcntl.fcntl(self._lock_fd, fcntl.F_SETFD, flags)
+
+        self._is_locked = False
+
+    def acquire(self, timeout=None):
+        """Acquires lock."""
+        assert self._lock_fd
+        assert not self._is_locked
+
+        lock_flag = fcntl.LOCK_EX | fcntl.LOCK_NB
+        start_time = time.time()
+        warned = False
+        while True:
+            try:
+                fcntl.lockf(self._lock_fd, lock_flag)
+                # Lock acquired successfully
+                self._is_locked = True
+                return self
+            except OSError as e:
+                if e.errno not in (errno.EAGAIN, errno.EACCES):
+                    raise
+
+                # Lock is held by another process, wait and retry
+                elapsed_time = time.time() - start_time
+
+                if elapsed_time > _WARN_SECONDS and not warned:
+                    print(f"WARNING: Still waiting on lock for generated "
+                          f"bazelrc directory. For details, run\n"
+                          f"    lsof {self.lockfile}",
+                          file=sys.stderr)
+                    warned = True
+
+                if timeout is not None and elapsed_time > timeout:
+                    raise TimeoutError
+
+                if elapsed_time > max(_WARN_SECONDS, timeout or 0):
+                    lock_flag = lock_flag & ~fcntl.LOCK_NB
+
+                # Sleep for a random number of seconds so we have some jittering
+                # and avoid too much contention.
+                time.sleep(random.random() * _WAIT_RECHECK_MAX_SECONDS)
+
+    def release(self):
+        """Releases lock."""
+        if self._lock_fd is not None:
+            os.close(self._lock_fd)
+        self._lock_fd = None
+
+        self._is_locked = False
+
+    @property
+    def is_locked(self) -> bool:
+        return self._lock_fd is not None and self._is_locked
 
 
 @dataclasses.dataclass
 class BazelrcWriter(object):
+    """Helper class to write bazelrc files.
+
+    Expected call sequence:
+
+    >>> gen_bazelrc_dir = pathlib.Path("/tmp/testdir")
+    >>> writer = BazelrcWriter(gen_bazelrc_dir)
+    >>> writer.acquire_lock()
+    >>> with writer.open_file() as f:
+    ...     _ = f.write("# content")
+    ...
+    >>> # Execute Bazel commands
+    >>> # If `bazel clean`, calls clean()
+    >>> writer.clean()
+    >>>
+    >>> # Release lock. If it is not called, lock is released upon process exit.
+    >>> writer.release_lock()
+    """
+
     gen_bazelrc_dir: pathlib.Path
 
     def __post_init__(self):
+        """Initializes the writer."""
         self.gen_bazelrc_dir.mkdir(parents=True, exist_ok=True)
 
-    def open(self) -> TextIO:
-        f = self.gen_bazelrc_dir / "generated.bazelrc"
+        self._lock = _Lockf(self.gen_bazelrc_dir / "lockfile")
+
+    @property
+    def _generated_file(self):
+        return self.gen_bazelrc_dir / "generated.bazelrc"
+
+    def open_file(self) -> TextIO:
+        """Opens the generated bazelrc file for writing.
+
+        Lock must be acquired before calling this method.
+        """
+        if not self._lock.is_locked:
+            raise ValueError(f"Lock not acquired for {self._generated_file}")
+
+        f = self._generated_file
         f.parent.mkdir(parents=True, exist_ok=True)
         return f.open("w")
 
     def clean(self):
-        """Deletes all generated bazelrc files and the lockfile.
+        """Delete all generated files.
 
-        Note: This must be executed AFTER the actual `bazel clean` has
-        finished:
-        - The bazelrc files are needed for `bazel clean` to be executed.
+        The lockfile cannot be deleted; see
+        BazelrcWriterTest.test_multi_process.
         """
-        shutil.rmtree(self.gen_bazelrc_dir, ignore_errors=True)
+        self._generated_file.unlink()
+
+    def acquire_lock(self, timeout=None):
+        """Acquires the lock.
+
+        On successful acquisition, prevents other `bazel.py` to proceed."""
+        self._lock.acquire(timeout)
+
+    def release_lock(self):
+        """Releases the lock.
+
+        This allows another `bazel.py` to proceed with modifying the bazelrc
+        file and running another bazel command.
+        """
+        self._lock.release()
